b657d0f1af963b9cb0a02c9848b365b2729266b2
Your GitHub Username <59120183+kevynjcg23@users.noreply.github.com>
2025-06-23 11:44:11 +0800

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+
+      output: 'export',
+  trailingSlash: true,
+  basePath: 'la-gold-tracker'
 };
 
 export default nextConfig;
