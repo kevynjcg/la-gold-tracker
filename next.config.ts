faa4f484974c3c03972284e9167f642b3f9e27ba
Your GitHub Username <59120183+kevynjcg23@users.noreply.github.com>
2025-06-23 12:14:56 +0800

    test

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-
-      output: 'export',
-  trailingSlash: true,
-  basePath: 'la-gold-tracker'
+  /* config options here */
 };
 
 export default nextConfig;
